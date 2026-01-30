b40a4fe231d9e280395056f49d20ccf14a808657
2413264 <rh1035@student.aru.ac.uk>
2026-01-30 15:31:23 +0000

    KAN-13 Added dynamic header loading from js

---
+++ scripts/common/header.js
@@ -0,0 +1,13 @@
+const header = document.createElement("header")
+
+header.innerHTML = `<div id="logo-div">
+            <div id="header-logo"></div>
+            <p>One
+And
+Done
+            </p>
+        </div>
+        <nav></nav>
+        <div id="sign-in-div"></div>`
+
+document.body.prepend(header)
