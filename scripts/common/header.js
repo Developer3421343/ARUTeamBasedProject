6719b158aaf56227623531743ee104b9c7c6c839
2413264 <rh1035@student.aru.ac.uk>
2026-02-01 20:30:35 +0000

    Basic prototype Basic prototype where a singular dropdown box is used to display the dropdown for all header navbar items

--- scripts/common/header.js
+++ scripts/common/header.js
@@ -1,17 +1,50 @@
 const header = document.createElement("header")
 
-header.innerHTML = `<div id="logo-div">
-            <div id="header-logo"></div>
-            <p>One
+header.innerHTML = `
+<div id="logo-div">
+    <div id="header-logo"></div>
+    <p>One
 And
 Done
-            </p>
-        </div>
-        <div id="header-dropdown-div">
-            <div id="header-dropdown-content"></div>
-            <nav>
-            </nav>
-        </div>
-        <div id="sign-in-div"></div>`
+    </p>
+</div>
+<nav id="header-dropdown-div">
+    <div id="header-dropdown-menu">
+        <button data-id="a" >Menu 1</button>
+        <button data-id="b" >Menu 2</button>
+        <button data-id="c" >Menu 3</button>
+    </div>
+
+    <div id="header-dropdown-content">
+        <ul data-id="a">
+            <li>Item 1A</li>
+            <li>Item 1B</li>
+        </ul>
+        <ul data-id="b">
+            <li>Item 2A</li>
+            <li>Item 2B</li>
+        </ul>
+        <ul data-id="c">
+            <li>Item 3A</li>
+            <li>Item 3B</li>
+        </ul>
+    </div>
+</nav>
+<div id="sign-in-div"></div>`
 
 document.body.prepend(header)
+
+document.querySelectorAll('#header-dropdown-menu [data-id]').forEach(el => {
+
+  el.addEventListener('mouseover', () => {
+    document
+      .querySelector(`#header-dropdown-content [data-id="${el.dataset.id}"]`)
+      ?.classList.add('header-dropdown-show');
+  });
+
+  el.addEventListener('mouseout', () => {
+    document
+      .querySelector(`#header-dropdown-content [data-id="${el.dataset.id}"]`)
+      ?.classList.remove('header-dropdown-show');
+  });
+});
