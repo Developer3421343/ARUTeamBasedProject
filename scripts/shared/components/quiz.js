323e50116483f4f68b120a6e4d0006ef1a769aa4
2413264 <rh1035@student.aru.ac.uk>
2026-02-27 23:12:10 +0000

    Created basic structure of quiz element

--- scripts/shared/components/quiz.js
+++ scripts/shared/components/quiz.js
@@ -1,4 +1,12 @@
-import sheet from "/stylesheets/shared/components/quiz.css" assert { type: "css" };
+const sheet = new CSSStyleSheet();
+
+async function loadCSS() {
+  const res = await fetch("/stylesheets/shared/components/quiz.css");
+  const css = await res.text();
+  sheet.replaceSync(css);
+}
+
+await loadCSS();
 
 class QuizElement extends HTMLElement {
 	constructor() {
@@ -7,9 +15,32 @@ class QuizElement extends HTMLElement {
 
 		shadow.adoptedStyleSheets = [sheet];
         shadow.innerHTML = `
-            <div class="quiz">
-                <button>Submit</button>
+            <b class="quiz-title">Quiz</b>
+            
+            <div id="score-div">
+            <p>Current score: 0/30</p>
+            <p>Attempts: 0</p>
             </div>
+
+            <hr>
+
+            <b class="question-header">Question 1</b><br>
+            <input type="radio" id="html" name="fav_language" value="HTML">
+            <label for="html">HTML</label><br>
+            <input type="radio" id="css" name="fav_language" value="CSS">
+            <label for="css">CSS</label><br>
+            <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+            <label for="javascript">JavaScript</label>
+
+            <hr>
+            
+            <b class="question-header">Question 1</b><br>
+            <input type="radio" id="html" name="fav_language" value="HTML">
+            <label for="html">HTML</label><br>
+            <input type="radio" id="css" name="fav_language" value="CSS">
+            <label for="css">CSS</label><br>
+            <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+            <label for="javascript">JavaScript</label>
         `;
 	}
 }
