eeae8b0df5faaed42de6956f41faae3195d0fe7c
2413264 <rh1035@student.aru.ac.uk>
2026-03-04 13:05:41 +0000

    Added quiz question popularion from parsed data

--- scripts/shared/components/quiz.js
+++ scripts/shared/components/quiz.js
@@ -1,9 +1,9 @@
 const sheet = new CSSStyleSheet();
 
 async function loadCSS() {
-  const res = await fetch("/stylesheets/shared/components/quiz.css");
-  const css = await res.text();
-  sheet.replaceSync(css);
+	const res = await fetch("/stylesheets/shared/components/quiz.css");
+	const css = await res.text();
+	sheet.replaceSync(css);
 }
 
 await loadCSS();
@@ -12,54 +12,70 @@ class QuizElement extends HTMLElement {
 	constructor() {
 		super();
 
-    class Question {
-      constructor(title, answers, correctIdx) {
-        this.title = title;
-        this.answers = answers;
-        this.correctIdx = correctIdx;
-      }
-    }
+		class Question {
+			constructor(title, answers, correctIdx) {
+				this.title = title;
+				this.answers = answers;
+				this.correctIdx = correctIdx;
+			}
+		}
 
-    // Parse questions
-    let questions = []
-    Array.from(element.children).filter((x) => x.tagName == "UL").forEach(child => {
-      let title = child.getAttribute("data-title") ?? "";
-      let answers = Array.from(child.children).map((x) => x.innerHTML)
-      let correctIdx = Array.from(child.children) .findIndex(el => el.dataset.correct === "true");
-      questions.push(new Question(title, answers, correctIdx));
-    });
+		// Parse questions
+		let questions = [];
+		Array.from(this.children)
+			.filter((x) => x.tagName == "UL")
+			.forEach((child) => {
+				let title = child.getAttribute("data-title") ?? "";
+				let answers = Array.from(child.children).map((x) => x.innerHTML);
+				let correctIdx = Array.from(child.children).findIndex((el) => el.dataset.correct === "true");
+				questions.push(new Question(title, answers, correctIdx));
+			});
+
+    // Clear innerHTML
+    this.innerHTML = "";
 
 		const shadow = this.attachShadow({ mode: "open" });
 
 		shadow.adoptedStyleSheets = [sheet];
-        shadow.innerHTML = `
+		shadow.innerHTML = `
             <b class="quiz-title">Quiz</b>
             
             <div id="score-div">
-            <p>Current score: 0/30</p>
-            <p>Attempts: 0</p>
-            </div>
+              <p>Current score: 0/30</p>
+              <p>Attempts: 0</p>
+            </div>`;
 
-            <hr>
+		for (let i = 0; i < questions.length; i++) {
+			// Get question
+			let q = questions[i];
 
-            <b class="question-header">Question 1</b><br>
-            <input type="radio" id="html" name="fav_language" value="HTML">
-            <label for="html">HTML</label><br>
-            <input type="radio" id="css" name="fav_language" value="CSS">
-            <label for="css">CSS</label><br>
-            <input type="radio" id="javascript" name="fav_language" value="JavaScript">
-            <label for="javascript">JavaScript</label>
+			// Add question title
+			shadow.append(
+				document.createElement("hr"),
+				Object.assign(document.createElement("b"), {
+					className: "question-header",
+					textContent: `Question ${i + 1}` + (q.title != "" ? ` - ${q.title}` : ""),
+				}),
+				document.createElement("br"),
+			);
 
-            <hr>
-            
-            <b class="question-header">Question 1</b><br>
-            <input type="radio" id="html" name="fav_language" value="HTML">
-            <label for="html">HTML</label><br>
-            <input type="radio" id="css" name="fav_language" value="CSS">
-            <label for="css">CSS</label><br>
-            <input type="radio" id="javascript" name="fav_language" value="JavaScript">
-            <label for="javascript">JavaScript</label>
-        `;    
+			// Populate answers
+			for (let aIdx = 0; aIdx < q.answers.length; aIdx++) {
+				let a = q.answers[aIdx];
+				shadow.append(
+					Object.assign(document.createElement("input"), {
+						type: "radio",
+						id: `radioset-q${i + 1}-a${aIdx + 1}`,
+						name: `radioset-q${i + 1}`,
+					}),
+					Object.assign(document.createElement("label"), {
+						htmlFor: `radioset-q${i + 1}-a${aIdx + 1}`,
+						innerHTML: a,
+					}),
+					document.createElement("br"),
+				);
+			}
+		}
 	}
 }
 
