d2fd21bdd90f0e78f4ee8f71a8a21238ec1c26b9
2513481 <nj433@student.aru.ac.uk>
2026-03-05 15:15:23 +0000

    added a prime number checker button on javascript

---
+++ scripts/NJ433-Lesson-1.js
@@ -0,0 +1,19 @@
+document.getElementById("is-it-a-prime-btn").addEventListener("click", () => {
+
+    let input = document.getElementById("is-it-a-prime-input").value;
+    let prime = true;
+
+    input = parseInt(input);
+    
+    if (typeof input !== "number" || isNaN(input)) {
+        return;
+    }
+
+    for(let i = 2, s = Math.sqrt(input); i <= s; i++) {
+        if(input % i === 0) prime = false;
+    }
+    if ([0, 1].includes(input)) prime = false;
+
+
+    document.getElementById("is-it-a-prime-output").textContent = prime ? input + " is a prime number." : input + " is not a prime number.";
+});
