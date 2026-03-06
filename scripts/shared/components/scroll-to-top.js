337073c07b27691227dccc6487889914eedae17e
2413264 <rh1035@student.aru.ac.uk>
2026-03-06 14:28:00 +0000

    Added button functionality

--- scripts/shared/components/scroll-to-top.js
+++ scripts/shared/components/scroll-to-top.js
@@ -10,11 +10,22 @@ await loadCSS();
 
 
 const host = document.createElement("div");
-host.id = "scroll-to-top"
 host.role = "button";
+host.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+})
+
+
+
+
 const shadow = host.attachShadow({ mode: "open" });
 
 shadow.adoptedStyleSheets = [sheet];
 shadow.innerHTML = "^";
 
 document.querySelector(".content").appendChild(host);
+
